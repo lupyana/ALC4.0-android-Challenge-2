29e4bf103a37117eae472ca47ff72a5a730a97c4
Lupyana <lupyanambembati@gmail.com>
2019-08-05 13:18:03 +0300

    Feat: Added push functionality to the app
    - Data is pushed to the firebase db

---
+++ app/src/main/java/co/tz/travelmantics/InsertActivity.java
@@ -0,0 +1,71 @@
+package co.tz.travelmantics;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class InsertActivity extends AppCompatActivity {
+
+    private FirebaseDatabase mfirebaseDatabase;
+    private DatabaseReference mDatabaseReference;
+
+    EditText txtTitle;
+    EditText txtDescription;
+    EditText txtPrice;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert);
+        mfirebaseDatabase = FirebaseDatabase.getInstance();
+        mDatabaseReference = mfirebaseDatabase.getReference().child("traveldeals");
+        txtTitle = findViewById(R.id.txtTitle);
+        txtDescription = findViewById(R.id.txtDescription);
+        txtPrice = findViewById(R.id.txtPrice);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.save_menu:
+                saveDeal();
+                Toast.makeText(this, "Deal Saved", Toast.LENGTH_LONG).show();
+                clean();
+                return true;
+             default:
+                 return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    private void clean() {
+        txtPrice.setText("");
+        txtTitle.setText("");
+        txtDescription.setText("");
+        txtTitle.requestFocus();
+    }
+
+    private void saveDeal() {
+        String title = txtTitle.getText().toString();
+        String description = txtDescription.getText().toString();
+        String price = txtPrice.getText().toString();
+        TravelDeal deal = new TravelDeal(title, description, price, "");
+        mDatabaseReference.push().setValue(deal);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.save_menu, menu);
+        return  true;
+    }
+}
